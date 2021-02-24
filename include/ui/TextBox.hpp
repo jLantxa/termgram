3b55b768d1eb5806e2b044447ebb0451b411af7f
Javier Lancha Vázquez <javier.lancha@gmail.com>
2021-02-24 01:20:10 +0100

    Add doxygen for the TextBox class

--- include/ui/TextBox.hpp
+++ include/ui/TextBox.hpp
@@ -29,20 +29,69 @@ public:
     TextBox(std::wstring title, unsigned int m_max_length);
     virtual ~TextBox();
 
+    /**
+     * \brief An event listener for the TextBox class
+     **/
     struct Listener {
+        /**
+         * \brief Callback for pressing the ENTER key.
+         * \param text_box A pointer to the TextBox that generated the event.
+         */
         virtual void OnEnterPressed(TextBox* text_box) = 0;
     };
 
+    /**
+     * \brief Input one character into the TextBox. It can be a textual character
+     * or an action character such as ENTER, BACKSPACE, DELETE, CTRL, etc...
+     */
     void Input(wint_t ch);
+
+    /**
+     * \brief Draw the TextBox widget into the screen.
+     */
     void Draw();
+
+    /**
+     * \brief A callback to handle teminal resize events.
+     */
     void OnTerminalResize();
 
+    /**
+     * \brief Return the std::wstring representation of the contents of the TextBox.
+     * \return TextBox content as a std::wstring
+     */
     std::wstring GetText() const;
 
+    /**
+     * \brief Sets the title of the TextBox.
+     * \param title A title represented as a std::wstring
+     */
     void SetTitle(std::wstring title);
+
+    /**
+     * \brief Set the maximum number of character the TextBox can hold.
+     * \param max_length Maximum number of characters. If set to 0,
+     * there will be no limit.
+     */
     void SetMaxLength(unsigned int max_length);
+
+    /**
+     * \brief Add a Listener for TextBox events. Listeners are stored in a list. If the listener
+     * already exists in the list it will not be added again.
+     * \param listener Pointer to a Listener
+     */
     void AddListener(Listener* listener);
+
+    /**
+     * \brief Remove a Listener from the list of Listeners. Listeners are only removed if they exist
+     * inside the list.
+     * \param listener Pointer to a Listener.
+     */
     void RemoveListener(Listener* listener);
+
+    /**
+     * \brief Clear the TextBox. It deletes the text, but the title is kept as is.
+     **/
     void Clear();
 
 private:
@@ -61,11 +110,24 @@ private:
 
     std::list<Listener*> m_listeners;
 
+    /**
+     * \brief Update the size of the TextBox
+     */
     void UpdateScreenBounds();
 
+    /**
+     * \brief Move one character to the left.
+     */
     void CursorLeft();
+
+    /**
+     * \brief Move one character to the right.
+     */
     void CursorRight();
 
+    /**
+     * \brief Draw the enclosing windo box
+     */
     void DrawBox();
 };
 
